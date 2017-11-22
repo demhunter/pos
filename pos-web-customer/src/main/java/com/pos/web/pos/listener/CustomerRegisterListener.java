ddd38f612eaf85dc840896d40db427a56de15ceb
王兵 <emp_kixs@163.com>
2017-11-22 13:59:16 +0800

    注册消息监听附加日志打印

--- pos-web-customer/src/main/java/com/pos/web/pos/listener/CustomerRegisterListener.java
+++ pos-web-customer/src/main/java/com/pos/web/pos/listener/CustomerRegisterListener.java
@@ -5,11 +5,14 @@ package com.pos.web.pos.listener;
 
 import com.pos.basic.mq.AbstractMQListener;
 import com.pos.basic.mq.MQMessage;
+import com.pos.common.util.basic.JsonUtils;
 import com.pos.common.util.validation.Preconditions;
 import com.pos.pos.service.PosService;
 import com.pos.user.dto.customer.CustomerDto;
 import com.pos.user.dto.mq.CustomerInfoMsg;
 import com.pos.user.service.CustomerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -24,6 +27,8 @@ import javax.annotation.Resource;
 @Component
 public class CustomerRegisterListener extends AbstractMQListener {
 
+    private final static Logger LOG = LoggerFactory.getLogger(CustomerRegisterListener.class);
+
     @Resource
     private PosService posService;
 
@@ -35,9 +40,12 @@ public class CustomerRegisterListener extends AbstractMQListener {
         Preconditions.checkArgsNotNull(message);
         CustomerInfoMsg msg = extractData(message, CustomerInfoMsg.class);
 
+        LOG.info("收到一条用户注册消息，msg={}", JsonUtils.objectToJson(msg));
         CustomerDto customerDto = customerService.findById(msg.getUserId(), false, false);
         if (customerDto != null) {
             posService.posLogin(customerDto, msg.getRecommendType(), msg.getRecommendId());
+        } else {
+            LOG.error("客户userId={}不存在！", msg.getUserId());
         }
 
         return true;
