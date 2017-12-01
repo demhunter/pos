0a3131b842dedf2bb341dedd227a055707209b03
王兵 <emp_kixs@163.com>
2017-12-01 15:55:43 +0800

    网关接口定义

---
+++ pos-web-gate/src/main/java/com/pos/web/gate/vo/ServerInfoVo.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2016 ywmj.com. All Rights Reserved.
+ */
+package com.pos.web.gate.vo;
+
+import com.wordnik.swagger.annotations.ApiModel;
+import com.wordnik.swagger.annotations.ApiModelProperty;
+
+import java.io.Serializable;
+
+/**
+ * 服务器信息VO
+ *
+ * @author wangbing
+ * @version 1.0, 2017/12/1
+ */
+@ApiModel("服务器信息")
+public class ServerInfoVo implements Serializable {
+
+    @ApiModelProperty("当前版本号")
+    private String currentVersion;
+
+    @ApiModelProperty("服务器的访问地址，即app访问的baseUrl")
+    private String serverUrl;
+
+    public String getCurrentVersion() {
+        return currentVersion;
+    }
+
+    public void setCurrentVersion(String currentVersion) {
+        this.currentVersion = currentVersion;
+    }
+
+    public String getServerUrl() {
+        return serverUrl;
+    }
+
+    public void setServerUrl(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+}
